8972c6f4499bc0ef39c20a390fe880608dd93e26
DevFlavioSantos <devflaviosantos@gmail.com>
2025-04-23 05:18:15 -0300

    Criação da paginação

--- src/main/java/com/ead/authuser/controller/UserController.java
+++ src/main/java/com/ead/authuser/controller/UserController.java
@@ -6,6 +6,10 @@ import com.ead.authuser.services.UserService;
 import com.fasterxml.jackson.annotation.JsonView;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -26,8 +30,10 @@ public class UserController {
     UserService userService;
 
     @GetMapping
-    public ResponseEntity<List<UserModel>> getAllUsers() {
-        return ResponseEntity.ok(userService.findAll());
+    public ResponseEntity<Page<UserModel>> getAllUsers(@PageableDefault(page = 0, size = 10, sort = "userId", direction = Sort.Direction.ASC)
+                                                           Pageable pageable) {
+        Page<UserModel>userModelsPage = userService.findAll(pageable);
+        return ResponseEntity.status(HttpStatus.OK).body(userModelsPage);
     }
 
     @GetMapping("/{userId}")
--- src/main/java/com/ead/authuser/services/UserService.java
+++ src/main/java/com/ead/authuser/services/UserService.java
@@ -2,6 +2,8 @@ package com.ead.authuser.services;
 
 import com.ead.authuser.models.UserModel;
 import net.bytebuddy.jar.asm.commons.Remapper;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 import java.util.Optional;
@@ -19,4 +21,6 @@ public interface UserService {
     boolean existsByUserName(String userName);
 
     boolean existsByEmail(String email);
+
+    Page<UserModel> findAll(Pageable pageable);
 }
